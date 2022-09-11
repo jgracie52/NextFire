4074b3574b89c43d1bcd10025db35ed0f67ce753
jgracie52 <46577150+jgracie52@users.noreply.github.com>
2022-09-11 10:20:35 -0400

    Version 1
    Completed Fireship.io tutorial on NextJS and Firebase

---
+++ nextfire/lib/firebase.js
@@ -0,0 +1,61 @@
+import firebase from 'firebase/compat/app'
+import 'firebase/compat/auth';
+import 'firebase/compat/firestore';
+import 'firebase/compat/storage';
+
+
+const firebaseConfig = {
+    apiKey: "AIzaSyApFzGjyey1qQhK4ovqeX5IyIr3aWGX26I",
+    authDomain: "nextfire-37f30.firebaseapp.com",
+    projectId: "nextfire-37f30",
+    storageBucket: "nextfire-37f30.appspot.com",
+    messagingSenderId: "951226678856",
+    appId: "1:951226678856:web:9d14dc15920ad280e7e27d",
+    measurementId: "G-STRXVYD79X"
+}
+
+if(!firebase.apps.lenght)
+{
+    firebase.initializeApp(firebaseConfig);
+}
+
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+export const firestore = firebase.firestore();
+
+// Storage exports
+export const storage = firebase.storage();
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+export const increment = firebase.firestore.FieldValue.increment;
+
+/// Helper Functions
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param {string} username
+ */
+export async function getUserWithUsername(username){
+    const usersRef = firestore.collection('users');
+    const query = usersRef.where('username', '==', username).limit(1);
+    const userDoc = (await query.get()).docs[0];
+    return userDoc;
+}
+
+/**`
+ * Convert a Firestore document to JSON
+ * @param {DocumentSnapshot} doc
+ */
+export function postToJSON(doc){
+    const data = doc.data();
+
+    return{
+        ...data,
+        // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+        createdAt: data.createdAt.toMillis(),
+        updatedAt: data.updatedAt.toMillis(),
+    };
+}
---
+++ nextfire/pages/index.js
@@ -0,0 +1,65 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/Home.module.css'
+import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
+import { firestore, postToJSON, fromMillis } from '../lib/firebase'
+import { useState } from 'react'
+import PostFeed from '../components/PostFeed'
+
+const LIMIT = 10;
+
+export async function getServerSideProps(context){
+  const postsQuery = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .limit(LIMIT);
+
+  const posts = (await (postsQuery.get())).docs.map(postToJSON);
+
+  return{
+    props: { posts }
+  };
+}
+
+export default function Home(props) {
+  const [posts, setPosts] = useState(props.posts);
+  const [loading, setLoading] = useState(false);
+  const [postsEnd, setPostsEnd] = useState(false);
+
+  const getMorePosts = async () => {
+    setLoading(true);
+    const last = posts[posts.length - 1];
+
+    const cursor = typeof last.createdAt === 'number' ? fromMillis(last.createdAt) : last.createdAt;
+    
+    const query = firestore
+    .collectionGroup('posts')
+    .where('published', '==', true)
+    .orderBy('createdAt', 'desc')
+    .startAfter(cursor)
+    .limit(LIMIT);
+
+    const newPosts = (await query.get()).docs.map((doc) => doc.data());
+
+    setPosts(posts.concat(newPosts));
+    setLoading(false);
+
+    if(newPosts.length < LIMIT){
+      setPostsEnd(true);
+    }
+  }
+
+  return (
+    <main>
+      <PostFeed posts={posts}></PostFeed>
+
+      {!loading && !postsEnd && <button onClick={getMorePosts}>Load More</button>}
+
+      <Loader show={loading}></Loader>
+
+      {postsEnd && 'You have reached the end!'}
+    </main>
+  )
+}
